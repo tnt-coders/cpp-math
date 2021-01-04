5bf4e4d2b7c999cbd031ad88a88e70ebb2a9adc8
Timothy Wrona <tjwrona1992@gmail.com>
2021-01-03 22:58:15 -0500

    Reformatted conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,18 +1,37 @@
 from conans import ConanFile, CMake, tools
 
 class CppMath(ConanFile):
-    author = "TNT Coders <tnt-coders@googlegroups.com>"
-    build_requires = "catch2/3.0.0-1@tnt-coders/stable"
-    default_options = {"shared": False}
-    description = "C++ library containing common math utility functions"
-    exports_sources = "CMakeLists.txt", "docs/*", "include/*", "src/*", "test/*"
-    generators = "cmake", "cmake_paths"
-    license = "GNU Lesser General Public License v3.0"
     name = "math"
-    options = {"shared": [True, False]}
-    settings = "os", "compiler", "build_type", "arch"
-    topics = ("math")
+    description = "C++ library containing common math utility functions"
+    homepage = "https://tnt-coders.github.io/"
     url = "https://github.com/tnt-coders/cpp-math"
+    license = "GNU Lesser General Public License v3.0"
+    author = "TNT Coders <tnt-coders@googlegroups.com>"
+
+    topics = ("math")
+
+    settings = ("os", "compiler", "build_type", "arch")
+    
+    options = {
+        "shared": [True, False],
+    }
+
+    default_options = {
+        "shared": False,
+    }
+
+    build_requires = (
+        "catch2/3.0.0-1@tnt-coders/stable",
+    )
+
+    exports_sources = ("CMakeLists.txt", "docs/*", "include/*", "src/*", "test/*")
+
+    generators = ("cmake", "cmake_paths")
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure()
+        return cmake
 
     def configure(self):
         tools.check_min_cppstd(self, "17")
@@ -28,8 +47,3 @@ class CppMath(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = self.cpp_info.libs = tools.collect_libs(self)
-
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure()
-        return cmake
